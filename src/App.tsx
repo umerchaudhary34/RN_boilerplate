a2b5296bacdc5cbc25df8f0d702fa13ef690c2a2
umer chaudhary <umerchaudhary34@gmail.com>
2023-02-13 06:22:47 +0500

    testing husky

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import {Text, View} from 'react-native';
 import Demo from './components/Demo';
 
-const App = () => {
-  return (
-    <View>
-      <Text>App</Text>
-      <Demo />
-    </View>
-  );
-};
+const App = () => (
+  <View>
+    <Text>App</Text>
+    <Demo />
+  </View>
+);
 export default App;
