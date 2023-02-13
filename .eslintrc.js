c4469c520a655e433005f9b4ee28087ba85656b0
umer chaudhary <umerchaudhary34@gmail.com>
2023-02-13 06:17:15 +0500

    Added eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community',
+    'plugin:prettier/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react/recommended',
+  ],
+  plugins: ['prettier', '@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    'react/jsx-uses-react': 1,
+    'arrow-body-style': ['error'],
+  },
 };
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import Demo from './components/Demo';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+      <Demo />
+    </View>
+  );
+};
+export default App;
